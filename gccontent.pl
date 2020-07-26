b3751f462e7da64cb142c19909e6baca7f6c6e91
can11sichuan <67733296+can11sichuan@users.noreply.github.com>
2020-07-26 18:03:42 +0800

    first_script.pl

--- gccontent.pl
+++ gccontent.pl
@@ -1,1 +1,15 @@
 
+#! /usr/bin/perl
+use strict;
+use warnings;
+use Bio::SeqIO;
+my $file=$ARGV[0];
+my $fa=Bio::SeqIO->new(-file=>$file,-format=>'fasta');
+while(my $seq_io=$fa->next_seq){
+    my $id=$seq_io->id;
+    my $seq=$seq_io->seq;
+		my $seq2=$seq;
+		$seq2=~s/A|T//gi;
+		my $percent= length($seq2)/length($seq);
+		print "$percent\n";
+}
